a746c590194da51d9e83725fc761b62012d35515
Dave Russell <daverussell1309@gmail.com>
2025-08-14 16:46:10 +0700

    remove unwanted log

--- internal/rest/midlleware/rate_limit.go
+++ internal/rest/midlleware/rate_limit.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"context"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -23,7 +22,6 @@ func RateLimit(rateLimiter RateLimiter, keyFunc func(*gin.Context) string) gin.H
 
 		allowed, err := rateLimiter.Allow(c.Request.Context(), clientID)
 		if err != nil {
-			log.Println("%v", err)
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "internal rate limiter error"})
 			c.Abort()
 			return
